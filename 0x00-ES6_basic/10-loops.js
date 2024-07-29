1fc8e681240311aa019db201837324a7ed5a8540
agaberr <ahmedgaber312001@gmail.com>
2024-07-30 01:06:18 +0300

    tasks

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
-    const newArray = [];
-    for (const value of array) {
-      newArray.push(appendString + value);
-    }
-  
-    return newArray;
+  const newArray = [];
+  // eslint-disable-next-line no-restricted-syntax
+  for (const value of array) {
+    newArray.push(appendString + value);
+  }
+
+  return newArray;
 }

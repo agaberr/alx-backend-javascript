2a0dc573ef58432345fdb6252e17330fce12646a
agaberr <ahmedgaber312001@gmail.com>
2024-07-30 19:29:11 +0300

    tasks

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,6 +1,6 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
   return students
-    .filter((student) => student.city === city)
+    .filter((student) => student.location === city)
     .map((student) => {
       const newGrade = newGrades.find(({ studentId }) => studentId === student.id);
       if (newGrade) {

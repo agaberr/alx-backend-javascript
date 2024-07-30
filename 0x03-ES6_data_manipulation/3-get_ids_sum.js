60599dba512693dc025e0926708a57559c86200c
agaberr <ahmedgaber312001@gmail.com>
2024-07-30 19:25:24 +0300

    tasks

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,6 +1,3 @@
 export default function getStudentIdsSum(getListStudents) {
-  if (!Array.isArray(getListStudents)) {
-    return [];
-  }
   return getListStudents.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 }
